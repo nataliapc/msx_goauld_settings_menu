4a3ff1604167f4d239d7386553b1e80ad87608af
NataliaPC <natalia.pujol@gmail.com>
2024-11-03 18:42:04 +0100

    printing menu optimized

--- src/menu.asm
+++ src/menu.asm
@@ -1,104 +1,88 @@
 org #7d40
-	call  CHSNS
-	ret z
-	call read_char
-	cp a,#67
-	ret nz
 
-	;load variables in ram
-	ld a,(var1_init)
-	ld (var1),a
-	ld a,(var2_init)
-	ld (var2),a
-	ld a,(var3_init)
-	ld (var3),a
-	ld a,(var4_init)
-	ld (var4),a
+;	call CHSNS
+;	ret z
+;	call read_char
+;	cp a,#67
+;	ret nz
 
-	;print menu
+	; Initialize RAM variables
+	ld hl, var_init_start
+	ld de, var_ram
+	ld bc, var_init_end-var_init_start
+	ldir
+
+	; Print menu
 	ld hl,#0101
 	call set_cursor
-	ld hl,menu
+	ld hl,menuStr
 	call print_string
 
-	;main loop
+; Main loop
 bucle:
-	;print var1
+	; Print var1
 	ld hl,#1402
-	call set_cursor
 	ld a,(var1)
-	ld hl,off
-	or a
-	jr z,sigue1
-	ld hl,on
-sigue1:
-	call print_string
+	call print_on_off
 
-	;print var2
+	; Print var2
 	ld hl,#1403
-	call set_cursor
 	ld a,(var2)
-	ld hl,off
-	or a
-	jr z,sigue2
-	ld hl,on
-sigue2:
-	call print_string
+	call print_on_off
 
-	;print var3
+	; Print var3
 	ld hl,#1404
-	call set_cursor
 	ld a,(var3)
-	ld hl,off
-	or a
-	jr z,sigue3
-	ld hl,on
-sigue3:
-	call print_string
+	call print_on_off
 
-	;print var4
+	; Print var4
 	ld hl,#1405
 	call set_cursor
 	ld a,(var4)
 	add a,#30
 	call print_char
 
-	call  CHSNS
+	; Wait for a key
+	call CHSNS
 	jr z, bucle
 	call read_char
 	or a
 	jr z, bucle
 
+	; Key '1' pressed
 	cp a,#31
 	jr nz,tecla2
-	
+
 	ld a,(var1)
 	xor 1
 	ld (var1),a
 	jr bucle
 
-tecla2:	
+tecla2:
+	; Key '2' pressed
 	cp a,#32
 	jr nz,tecla3
-	
+
 	ld a,(var2)
 	xor 1
 	ld (var2),a
 	jr bucle
 
 tecla3:
+	; Key '3' pressed
 	cp a,#33
 	jr nz,tecla4
-	
+
 	ld a,(var3)
 	xor 1
 	ld (var3),a
 	jr bucle_largo
 
 tecla4:
+	; Key '4' pressed
 	cp a,#34
 	jr nz,tecla5
-	
+
 	ld a,(var4)
 	inc a
 	cp a,4
@@ -106,9 +90,10 @@ tecla4:
 	xor a
 no4:
 	ld (var4),a
-	jr bucle_largo	
+	jr bucle_largo
 
 tecla5:
+	; Key '5' pressed
 	cp a,#35
 	jr nz,tecla6
 	call get_config
@@ -116,12 +101,13 @@ tecla5:
 	ret
 
 tecla6:
+	; Key '6' pressed
 	cp a,#36
 	jr nz,bucle_largo
 	call get_config
 	or #80
 	out (#41),a
-	ret	
+	ret
 
 bucle_largo:
 	jp bucle
@@ -147,37 +133,83 @@ get_config:
 	or b
 	ret
 
-;msx
-print_char equ #00a2
-set_cursor equ #00C6
-read_char equ #009f
-CHSNS equ #009C
-
-;amstrad
-;print_char equ #bb5a
-;set_cursor equ #bb75
-;read_char equ #bb09
-
+; Prints characters from memory until a 0 is found.
+; Input    : HL - The text address 
 print_string:
 	ld a,(hl)
-	cp 255
+	or a
 	ret z
 	inc hl
 	call print_char
 	jr print_string
 
+; Set the cursor to L,H position and prints 'Off'/'On ' if A is 0 or not.
+; Input    : H  - Y coordinate of cursor
+;            L  - X coordinate of cursor
+;            A  - Value to print (0:Off 1:On)
+print_on_off:
+	ld   b, a
+	call set_cursor
+	ld   a, b
+	ld hl,offStr
+	or a
+	jr z,print_on_off_end
+	ld hl,onStr
+print_on_off_end:
+	call print_string
+	ret
 
-var1_init: db 1
-var2_init: db 1
-var3_init: db 1
-var4_init: db 0
+; ############## Variables
 
-var1 equ #8000
-var2 equ #8001
-var3 equ #8002
-var4 equ #8003
+var_init_start:
+	var1_init: db 1
+	var2_init: db 1
+	var3_init: db 1
+	var4_init: db 0
+var_init_end:
 
+var_ram equ #8000
+var1 equ var_ram+0
+var2 equ var_ram+1
+var3 equ var_ram+2
+var4 equ var_ram+3
 
-menu: db "Goauld Config",#0d,#0a,"1-Enable Mapper",#0d,#0a,"2-Enable Megaram",#0d,#0a,"3-Enable Scanlines",#0d,#0a,"4-Mapper Slot",#0d,#0a,"5-Save & Exit",#0d,#0a,"6-Save & Reset",255
-on: db "On ",255
-off: db "Off",255
+; ############## Constants
+
+menuStr:
+	db "Goauld Config",#0d,#0a
+	db "1-Enable Mapper",#0d,#0a
+	db "2-Enable Megaram",#0d,#0a
+	db "3-Enable Scanlines",#0d,#0a
+	db "4-Mapper Slot",#0d,#0a
+	db "5-Save & Exit",#0d,#0a
+	db "6-Save & Reset",0
+onStr: db "On ",0
+offStr: db "Off",0
+
+; ############## MSX BIOS
+
+; ####### CHPUT
+; Address  : #00A2
+; Function : Displays one character
+; Input    : A  - ASCII code of character to display
+print_char equ #00a2
+; ####### POSIT
+; Address  : #00C6
+; Function : Moves cursor to the specified position
+; Input    : H  - Y coordinate of cursor
+;            L  - X coordinate of cursor
+; Registers: AF
+set_cursor equ #00c6
+; ####### CHGET
+; Address  : #009F
+; Function : One character input (waiting)
+; Output   : A  - ASCII code of the input character
+; Registers: AF
+read_char equ #009f
+; ####### CHSNS
+; Address  : #009C
+; Function : Tests the status of the keyboard buffer
+; Output   : Zero flag set if buffer is empty, otherwise not set
+; Registers: AF
+CHSNS equ #009C
